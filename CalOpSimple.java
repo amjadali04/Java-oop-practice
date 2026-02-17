f1b7a1fc5d09b7fbc76bb99074e2cfc23ad5413d
Amjad ali <amjadbhuttof@gmail.com>
2026-02-17 15:33:07 +0500

    Add files via upload

---
+++ CalOpSimple.java
@@ -0,0 +1,12 @@
+public class CalOpSimple {
+    public static void main(String[] args) {
+        int num1 = 9;
+        int num2 = 3;
+        System.out.println("addition is : "+(num1+num2));
+        System.out.println("subtraction is : "+(num1-num2));
+        System.out.println("devision is : "+(float)(num1/num2));
+        System.out.println("multiplication is : "+(num1*num2));
+    
+    }
+    }
+      
