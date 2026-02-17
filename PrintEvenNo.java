1ac562e15ab1b856455301f838fc4b3e98dae083
Amjad ali <amjadbhuttof@gmail.com>
2026-02-17 15:39:02 +0500

    Add files via upload

---
+++ PrintEvenNo.java
@@ -0,0 +1,11 @@
+public class PrintEvenNo {
+    public static void main(String[] args) {
+    System.out.println("even no from 1 to 50 :  ");
+        for(int i = 1; i<=50; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
