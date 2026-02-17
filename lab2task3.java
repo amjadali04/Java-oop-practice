dcccd0787b08069cfee9aa5bcabd2f87f9bb29f3
Amjad ali <amjadbhuttof@gmail.com>
2026-02-17 16:07:31 +0500

    Add files via upload

---
+++ lab2task3.java
@@ -0,0 +1,9 @@
+public class lab2task3 {
+      public static void main(String[] args) {
+        int Length=9;
+        float Width=16.4f;
+        float area=Length*Width;
+        System.out.println("Area is "+area);
+    }
+    
+}
